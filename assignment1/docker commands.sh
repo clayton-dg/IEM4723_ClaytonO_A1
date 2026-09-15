19131ea236138ad36169eb1efae04de16eb35876
clayton-dg <clolsen@okstate.edu>
2026-09-14 23:18:20 -0500

    Q2 Volume Mounting Complete

--- assignment1/docker commands.sh
+++ assignment1/docker commands.sh
@@ -6,7 +6,8 @@ docker tag linear-regression-app:latest claytondg/linear-regression-app:latest
 docker push claytondg/linear-regression-app:latest
 
 # Question 2
-
+docker build -t volume-mounting .
+docker run --volume "C:\Users\clayt\OneDrive\Documents\Info Sytstems\Info Sys A1\IEM4723_ClaytonO_A1\assignment1\volume-mounting\data:/home/iem4723/data" volume-mounting
 # Question 3
 
 # Question 4
