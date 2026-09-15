09d01e559341199ba696fa28a9fa4e9a959a97e3
clayton-dg <clolsen@okstate.edu>
2026-09-15 17:54:56 -0500

    Q3 Complete

--- assignment1/docker commands.sh
+++ assignment1/docker commands.sh
@@ -8,6 +8,9 @@ docker push claytondg/linear-regression-app:latest
 # Question 2
 docker build -t volume-mounting .
 docker run --volume "C:\Users\clayt\OneDrive\Documents\Info Sytstems\Info Sys A1\IEM4723_ClaytonO_A1\assignment1\volume-mounting\data:/home/iem4723/data" volume-mounting
+
 # Question 3
+docker build -t logistic_regression_layered:latest .
+docker run --volume "C:\clayt\OneDrive\Documents\Info Systems\Info Sys A1\IEM4723_ClaytonO_A1\assignment1\layering\data:/home/iem4723/data:home/iem4723/data" logistic_regression_layered:latest python3 /home/iem4723/LogisticRegressor.py
 
 # Question 4
