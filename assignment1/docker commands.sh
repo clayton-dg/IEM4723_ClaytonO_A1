be01a86ceb2dc5e644c030db71fcb80be630b337
clayton-dg <clolsen@okstate.edu>
2026-09-14 22:26:58 -0500

    Q1 building tagging and pushing complete

---
+++ assignment1/docker commands.sh
@@ -0,0 +1,12 @@
+# Question 1
+docker build -t linear-regression-app .
+docker tag linear-regression-app:latest linear-regression-app:v0.1.1
+docker tag linear-regression-app:latest linear-regression-app:v0.1.2
+docker tag linear-regression-app:latest claytondg/linear-regression-app:latest
+docker push claytondg/linear-regression-app:latest
+
+# Question 2
+
+# Question 3
+
+# Question 4
